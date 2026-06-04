e1020ad8e11461f2913ddaf71cbeb4ba4c5fb484
Quiniela <quiniela@app.com>
2026-06-04 20:02:12 +0000

    style: Barlow Condensed + Jakarta Sans, verde #11503E México, año 2026

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,23 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        display: ['Syne', 'sans-serif'],
-        body: ['DM Sans', 'sans-serif'],
+        display: ['Barlow Condensed', 'sans-serif'],
+        body: ['Plus Jakarta Sans', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
       },
       colors: {
         pitch: {
-          50: '#f0fdf4',
+          50:  '#f0fdf4',
           100: '#dcfce7',
           200: '#bbf7d0',
           300: '#86efac',
           400: '#4ade80',
-          500: '#22c55e',
-          600: '#16a34a',
-          700: '#15803d',
-          800: '#166534',
-          900: '#14532d',
-          950: '#052e16',
+          500: '#17654E',
+          600: '#11503E',
+          700: '#0d3d30',
+          800: '#092b21',
+          900: '#061a14',
+          950: '#030d09',
         },
         gold: {
           400: '#fbbf24',
