2deaf06f1c9143d344593a91555265c8681e022a
danielap-netizen <danielap@din.com.mx>
2026-06-29 00:13:51 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import PartidosPage from './pages/PartidosPage'
 import AdminPage from './pages/AdminPage'
 import ParticipantesPage from './pages/ParticipantesPage'
 
+import OctavosPage from './pages/OctavosPage'
+import AdminOctavosPage from './pages/AdminOctavosPage'
+import TablaEliminatoriasPage from './pages/TablaEliminatoriasPage'
+
 function PrivateRoute({ children }) {
   const { user, loading } = useAuth()
 
@@ -100,6 +104,17 @@ export default function App() {
           />
 
           <Route
+            path="/tabla-eliminatorias"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <TablaEliminatoriasPage />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+
+          <Route
             path="/partidos"
             element={
               <Layout>
@@ -118,11 +133,33 @@ export default function App() {
           />
 
           <Route
+            path="/octavos"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <OctavosPage />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+
+          <Route
             path="/admin"
             element={
               <PrivateRoute>
                 <Layout>
                   <AdminPage />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/admin-octavos"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <AdminOctavosPage />
                 </Layout>
               </PrivateRoute>
             }
